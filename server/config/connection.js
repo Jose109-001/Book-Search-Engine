7a03af3a531bf3dee20cd04f3ab473c8c2f64510
Jose <josephgaspar725@gmail.com>
2021-07-27 09:35:55 +1000

    graphql added to nodem

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+mongoose.connect(
+  'mongodb+srv://joseph:DRUNJGW9BrfxgTA7@cluster0.bpxwl.mongodb.net/booksearchengine?retryWrites=true&w=majority'
+  || process.env.MONGODB_URI
+  || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
